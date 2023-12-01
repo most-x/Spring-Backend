80dbe5ab359e114d1d5a7578114e802f18d77879
Most-X <web-master@mostx.co.kr>
2023-12-01 13:41:43 +0900

    자산검색관련 API
    자산코드 검색 API
    자산 검색 API 테스트

--- src/main/java/com/example/demo/controller/TestJpaRestController.java
+++ src/main/java/com/example/demo/controller/TestJpaRestController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Asset;
 import com.example.demo.repository.AssetRepository;
 import com.example.demo.response.Response;
 import com.example.demo.service.AssetService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -23,7 +26,27 @@ public class TestJpaRestController {
     @Operation(summary = "전체 자산 조회", description = "전체 자산을 조회한다.")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/assets")
-    public Response<?> findAll(){
-        return new Response<>("true", "조회 성공", assetService.findAll());
+    public Response<?> findAll(Pageable pageable){
+        return new Response<>("true", "조회 성공", assetService.findAll(pageable));
+    }
+
+    @Operation(summary = "자산번호 검색", description = "WRMS에 등록된 자산코드로 조회한다.")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/assets/asset-code/{wrmsAssetCode}")
+    @Parameter(name="wrmsAssetCode", description = "WRMS 자산코드", example = "TEST000000001")
+    public Response<?> findAssetCode(@PathVariable("wrmsAssetCode") String wrmsAssetCode){
+        Asset asset = assetService.findAssetCode(wrmsAssetCode);
+        return new Response<>("true", "조회 성공", assetService.findAssetCode(wrmsAssetCode));
+    }
+
+    @GetMapping("/assets/asset-search")
+    public Response<?> findSearchAsset(@RequestParam(required = false) String wrmsAssetCode, @RequestParam(required = false) String wrmsItemCode, @RequestParam(required = false) String ilsangProductCode,
+                                       @RequestParam(required = false) String serialNumber, @RequestParam(required = false) String productName,
+                                       @RequestParam(required = false) String assetStatus, @RequestParam(required = false) String assetUsage, @RequestParam(required = false) Integer startPrice,
+                                       @RequestParam(required = false) Integer endPrice, @RequestParam(required = false) Date initialStartDate, @RequestParam(required = false) String warehouseNumber,
+                                       Pageable pageable) {
+
+        return new Response<>("true", "조회 성공", assetService.findSearchAsset(wrmsAssetCode, wrmsItemCode, ilsangProductCode, serialNumber, productName,
+                assetStatus, assetUsage, startPrice, endPrice, initialStartDate, warehouseNumber, pageable));
     }
 }
