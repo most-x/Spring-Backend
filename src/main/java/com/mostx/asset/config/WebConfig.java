ed248ca1f896c05468cb55c93563dfe041ba3d07
mostx <web-master@mostx.co.kr>
2024-04-11 17:12:21 +0900

    cross origin 수정 -> 전역설정 수정

--- src/main/java/com/mostx/asset/config/WebConfig.java
+++ src/main/java/com/mostx/asset/config/WebConfig.java
@@ -4,16 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-//                .allowedHeaders("Authorization", "Content-Type")
-//                .exposedHeaders("Custom-Header")
-//                .allowCredentials(true)
-//                .maxAge(3600);
-//    }
-//}
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://japi.mostx.co.kr", "https://japi.mostx.co.kr")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
