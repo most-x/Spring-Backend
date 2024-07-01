8be7168a55d570a962008aa20ab4f76ab2090854
mostx <web-master@mostx.co.kr>
2024-07-01 17:07:02 +0900

    신규 Branch생성 - dev Branch commit 후 main merge

--- src/main/java/kr/co/mostx/japi/repository/ServeyCustomRepository.java
+++ src/main/java/kr/co/mostx/japi/repository/ServeyCustomRepository.java
@@ -1,10 +1,23 @@
 package kr.co.mostx.japi.repository;
 
 import kr.co.mostx.japi.entity.Servey;
+import kr.co.mostx.japi.response.ServeyResponse;
 
 import java.util.List;
 
 public interface ServeyCustomRepository {
     List<Servey> searchServey(String serveyNumber);
+
+    ServeyResponse weeklyStaticsServey();
+
+    // 월간 통계 데이터
+    ServeyResponse monthlyStaticsServey();
+
+    // 점수 통계 데이터
+    ServeyResponse scoreStaticsServey();
+
+    ServeyResponse consultantStaticsServey();
+
+    ServeyResponse dailyStaticsServey();
 }
 
